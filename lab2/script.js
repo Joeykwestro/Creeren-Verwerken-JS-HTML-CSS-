28ab89c5397fda9702834a09c946c005f584bb30
Joey <99062903@mydavinci.nl>
2021-01-18 14:11:02 +0100

    .

--- lab2/script.js
+++ lab2/script.js
@@ -6,7 +6,10 @@ for(i = 1; i<= aantal; i++){
 	arr.push(prompt("Welke naam wilt u toevoegen?"));
 }
 
+var h2 = document.createElement("h2");
+document.body.appendChild(h2);
+
 for(i = 0; i<=number; i++){
-	document.write(arr[i] + "<br>");
+	h2.innerHTML = h2.innerHTML + "<br>" + arr[i];
 }
 
---
+++ lab4/A/script.js
@@ -0,0 +1,14 @@
+var container = document.createElement("div");
+var text1 = document.createElement("p");
+var input1 = document.createElement("input");
+var text2 = document.createElement("p");
+var input2 = document.createElement("input");
+var button = document.createElement("button");
+
+document.body.appendChild(container);
+container.appendChild(text1);
+container.appendChild(input1);
+container.appendChild(text2);
+container.appendChild(input2);
+container.appendChild(button);
+
